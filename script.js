b45bfa79e632321636df9ba9f24695da8ed90222
thaddeusmbrown <thaddeusmbrown@gmail.com>
2021-12-22 20:15:24 -0600

    finished basic functionality of the calculator and started working on extra credit

--- script.js
+++ script.js
@@ -25,22 +25,69 @@ const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 //figure out if user is typing out the next number or adding an operation to the stack
 let stack = []
+let equalHold = 0; //this bit will flip after hitting equals, so that starting with a new number will clear the stack
+let decimalHold = 0; //this bit will flip if a decimal is already in the number, to prevent adding multiple decimals
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(typeof parseInt(button.id));
-        if (typeof parseInt(button.id) == "number") {
-            if (stack.length == 0) {
+        if (parseInt(button.innerText)+1) { //if (a number)
+            if (stack.length == 0 || equalHold) { //if the stack is empty or an equation was just completed
                 stack[0] = button.id;
-                display.textContent = stack[0];
-            } else if (typeof parseInt(stack[0]) == "number") {
-                stack[0] = [stack[0],button.id].join('');
-                display.textContent = stack[0];
+                equalHold = 0;
+                display.textContent = stack.join(' ').replace('add', '+').replace('subtract', '-').replace('multiply', 'x').replace('divide', '÷');
+            } else if (parseInt(stack[stack.length-1])) { //if (an operator was clicked), parseInt converts operator to 'NaN' 
+                stack[stack.length-1] = [stack[stack.length-1],button.id].join('');
+                equalHold = 0;
+                display.textContent = stack.join(' ').replace('add', '+').replace('subtract', '-').replace('multiply', 'x').replace('divide', '÷');
             } else {
-                stack.shift(button.id);
-                display.textContent = stack[0];
+                stack.push(button.id);
+                equalHold=0;
+                display.textContent = stack.join(' ').replace('add', '+').replace('subtract', '-').replace('multiply', 'x').replace('divide', '÷');
             }
-        } else if {button.id == ''}
+        } else if (stack.length != 0 && button.id == 'add' || button.id == 'subtract' || button.id == 'multiply' || button. id == 'divide') {
+            if (parseInt(stack[stack.length-1])) {
+                stack.push(button.id);
+                equalHold=0;
+                display.textContent = stack.join(' ').replace('add', '+').replace('subtract', '-').replace('multiply', 'x').replace('divide', '÷');
+            } else {
+                stack[stack.length-1]=button.id;
+                display.textContent = stack.join(' ').replace('add', '+').replace('subtract', '-').replace('multiply', 'x').replace('divide', '÷');
+            }
+        } else if (button.id == 'clear') {
+            stack = [];
+            equalHold = 0;
+            display.textContent = stack;
+        } else {
+            if (!parseInt(stack[stack.length-1])) {
+                stack.pop();
+            }
+            while(stack.length>1) {
+                let firstNum = parseInt(stack.shift());
+                console.log(typeof stack[stack.length-1]);
+                let operation = stack.shift();
+                let secondNum = parseInt(stack.shift());
+                if (operation == 'add') {
+                    stack.unshift(operate(add, firstNum, secondNum).toString());
+                } else if (operation == 'subtract') {
+                    stack.unshift(operate(subtract, firstNum, secondNum).toString());
+                } else if (operation == 'multiply') {
+                    stack.unshift(operate(multiply, firstNum, secondNum).toString());
+                }  else {
+                    if (!secondNum) {
+                        stack=secondNum;
+                        break;
+                    } else {
+                        stack.unshift(operate(divide, firstNum, secondNum).toString());
+                    }
+                }
+            }
+            if(!stack) {
+                display.textContent = "Watch out for zero!";
+                stack = [];
+            } else {
+                display.textContent = Math.floor(stack[0]*1000) / 1000;
+                stack[0] = Math.floor(stack[0]*1000) / 1000;
+                equalHold = 1;
+            }
         }
-
-    })
-})
+    });
+});
